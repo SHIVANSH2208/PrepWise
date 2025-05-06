11386477fc2799a7b20a1329269e466a92d883e1
SHIVANSH2208 <shivanshtiwari220803@gmail.com>
2025-05-07 01:00:19 +0530

    Safe

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -81,7 +81,6 @@ export async function setSessionCookie(idToken: string) {
         path: '/',
         sameSite: 'lax',
     })
-        
 }
 
 export async function getCurrentUser(): Promise<User | null> {
@@ -112,4 +111,34 @@ export async function isAuthenticated() {
     const user = await getCurrentUser();
 
     return !!user;
+}
+
+export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
+    const interviews= await db
+    .collection('interviews')
+    .where('userId', '==', userId)
+    .orderBy('createdAt', 'desc')
+    .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
+}
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
+    const { userId, limit = 20 } = params;
+    
+    const interviews= await db
+    .collection('interviews')
+    .orderBy('createdAt', 'desc')
+    .where('finalized', '==', true)
+    .where('userId', '!=', userId)
+    .limit(limit)
+    .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
 }
